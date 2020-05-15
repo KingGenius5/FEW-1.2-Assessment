7b549012d3a94b9e2dc87ab7bcefb0e9b45fcc20
Muhammad Tifak <moht20009@gmail.com>
2020-05-15 03:49:19 -0700

    Completed working product
    The majority of challenges have been completed, will go back and attempt the rest as well as add more styling.

---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Heading.css';
+
+function PageHeader() {
+    return (
+        <div className="header">
+            <h1>FEW 1.2 Final Assessment</h1>
+            <h2>Muhammad Tifak</h2>
+        </div>
+    )
+}
+
+export default PageHeader;
---
+++ src/like.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import './like.css'
+
+class Clicker extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { count: 0 }
+    }
+
+    increment() {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    decrement() {
+        this.setState({ count: this.state.count - 1 })
+    }
+
+    render() {
+        return (
+            <div className='count'>
+
+                <button onClick={(e) => {
+                    this.decrement()
+                }}>-</button>
+
+                <h1>{this.state.count}</h1>
+
+                <button onClick={(e) => {
+                    this.increment()
+                }}>+</button>
+
+            </div>
+        )
+    }
+}
+
+export default Clicker;
